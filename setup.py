fec154cc5e9eca55a4f2f5452eb29eb0d8f9b40b
Vizonex <114684698+Vizonex@users.noreply.github.com>
2023-06-07 15:34:10 -0500

    Update setup.py
    Update setup.py to start allowing users to install onto their systems

--- setup.py
+++ setup.py
@@ -1,31 +1,82 @@
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 import pathlib 
+import sys 
 
+
+HERE = pathlib.Path("winloop")
+long_description = open('readme.md').read()
+
+__version__ = "0.0.1"
 
 
 
 # This is a temporary test Solution and is not the official file yet but this is to display/show what 
 # I'm currently using to compile the winloop library...
+def do_installation():
 
-current = pathlib.Path(__file__).parent / "winloop"
-Winlib = pathlib.Path("C:\\Program Files (x86)\\Windows Kits\\10\\Lib\\10.0.19041.0\\um\\x64")
 
-ext = [
-    Extension("winloop.loop",["winloop\\loop.pyx"],extra_link_args=[
-        "winloop\\vendor\\uv_a.lib",
-        str(Winlib / "Shell32.lib"),
-        str(Winlib / "Ws2_32.lib"),
-        str(Winlib / "Advapi32.lib"),
-        str(Winlib / "iphlpapi.lib"),
-        str(Winlib / "WSock32.lib"),
-        str(Winlib / "Userenv.lib"),
-        str(Winlib / "User32.lib")
+    # "Thank goodness we don't have to dig out the libpath ourselves I almost vomited" - Vizonex            
+    Windows_Libraries = [
+        "Shell32.lib",
+        "Ws2_32.lib",
+        "Advapi32.lib",
+        "iphlpapi.lib",
+        "WSock32.lib",
+        "Userenv.lib",
+        "User32.lib"
+    ]
+
+    ext = [
+        Extension("winloop.loop",["winloop\\loop.pyx"], 
+            # NOTE uv_a.lib will be user-compiled when 
+            # I've fixed the install there's still been some probelms with compiling fs-poll.c on it's own...
+            extra_link_args=Windows_Libraries + ["winloop\\vendor\\uv_a.lib"],
+            # I have some macros setup for now to help me with debugging - Vizonex
+            library_dirs=["winloop"],
+            define_macros=[
+                ('_GNU_SOURCE', 1),
+                ('WIN32', 1),
+                ("_WIN32_WINNT","0x0602")
+            ],
+            # Just incase, Try Optimzing a bit further....
+            extra_compile_args=["/O2"]
+        )
+    ]
+
+
+    setup(
+        name="winloop.loop",
+        author="Vizonex",
+        version=__version__,
+        description="""An Alternative library for uvloop compatability with windows""",
+        ext_modules=cythonize(ext),
+        license="MIT",
+        platforms=['Microsoft Windows'],
+        keywords=[
+            "winloop", 
+            "libuv", 
+            "windows", 
+            "cython",
+            "fast-asyncio",
+            "uvloop-alternative"
         ],
-        # I have some macros setup for now to help me with debugging - Vizonex
-        library_dirs=["winloop"],define_macros=[('_GNU_SOURCE', 1),('WIN32', 1),("UVLOOP_DEBUG", 1)]
+        long_description_content_type='text/markdown',
+        long_description=long_description,
+        classifiers=[
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
+            'Intended Audience :: Developers',
+            'Framework :: AsyncIO'
+        ],
     )
-]
 
+if __name__ == "__main__":
 
-setup(name="winloop.loop",ext_modules=cythonize(ext))
+    if sys.platform not in ('win32', 'cygwin', 'cli'):
+        # TODO (Vizonex) Ask uvloop Owners/contributors if it would be ok or acceptable to recommend 
+        # this library as an alternative resource for any users on windows 
+        raise RuntimeError("Winloop is Only Avalible for Windows Users Please try installing uvloop instead, you won't be dissapointed with it...")
+
+    do_installation()
