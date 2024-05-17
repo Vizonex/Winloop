7248864c211a5ef36e7e1d2c1e6a05e79f02b187
Berry Schoenmakers <l.a.m.schoenmakers@tue.nl>
2024-05-17 13:56:30 +0200

    Introduce PLATFORM_IS_WINDOWS.
    Towards unifying includes/compat.h for uvloop and winloop.

--- winloop/includes/compat.h
+++ winloop/includes/compat.h
@@ -1,38 +1,63 @@
-#ifndef _WINLOOP_CONTEXT_H_
-#define _WINLOOP_CONTEXT_H_
-
-/* Used to help pass along python's api to Contextvar's features in C...*/
 #include "Python.h"
 
- 
+
+#ifdef __APPLE__
+#define PLATFORM_IS_APPLE 1
+#else
+#define PLATFORM_IS_APPLE 0
+#endif
+
+
+#ifdef __linux__
+#  define PLATFORM_IS_LINUX 1
+#  include <sys/epoll.h>
+#else
+#  define PLATFORM_IS_LINUX 0
+#endif
+
+#ifdef __APPLE__
+#  define EPOLL_CTL_DEL 2
+struct epoll_event {};
+int epoll_ctl(int epfd, int op, int fd, struct epoll_event *event) {
+    return 0;
+};
+#endif
+
+/* TODO: add missing parts of uvloop/includes/compat.h */
+
+#ifdef _WIN32
+#define PLATFORM_IS_WINDOWS 1
+#else
+#define PLATFORM_IS_WINDOWS 0
+#endif
+
+
 #if PY_VERSION_HEX < 0x03070100
 
-    PyObject * Context_CopyCurrent(void) {
-        return (PyObject *)PyContext_CopyCurrent();
-    };
+PyObject * Context_CopyCurrent(void) {
+    return (PyObject *)PyContext_CopyCurrent();
+};
 
-    int Context_Enter(PyObject *ctx) {
-        return PyContext_Enter((PyContext *)ctx);
-    }
+int Context_Enter(PyObject *ctx) {
+    return PyContext_Enter((PyContext *)ctx);
+}
 
-    int Context_Exit(PyObject *ctx) {
-        return PyContext_Exit((PyContext *)ctx);
-    }
+int Context_Exit(PyObject *ctx) {
+    return PyContext_Exit((PyContext *)ctx);
+}
 
 #else
 
-    PyObject * Context_CopyCurrent(void) {
-        return PyContext_CopyCurrent();
-    };
+PyObject * Context_CopyCurrent(void) {
+    return PyContext_CopyCurrent();
+};
 
-    int Context_Enter(PyObject *ctx) {
-        return PyContext_Enter(ctx);
-    }
+int Context_Enter(PyObject *ctx) {
+    return PyContext_Enter(ctx);
+}
 
-    int Context_Exit(PyObject *ctx) {
-        return PyContext_Exit(ctx);
-    }
+int Context_Exit(PyObject *ctx) {
+    return PyContext_Exit(ctx);
+}
 
-
-#endif  
-#endif /* _WINLOOP_CONTEXT_H_ */
+#endif
