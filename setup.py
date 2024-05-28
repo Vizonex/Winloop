1588c970f12f40211867d624ed854724e2d514b3
Berry Schoenmakers <l.a.m.schoenmakers@tue.nl>
2024-05-28 08:32:22 +0200

    Slight update setup.

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
+import sys
+
+vi = sys.version_info
+if vi < (3, 8):
+    raise RuntimeError('winloop requires Python 3.8 or greater')
+
+if sys.platform not in ('win32', 'cygwin', 'cli'):
+    raise RuntimeError('winloop only supports Windows at the moment,'
+                       ' install uvloop on Linux, MacOS, etc.')
+
 from setuptools import Extension, setup, find_packages
 import pathlib
 import sys
@@ -7,17 +17,6 @@ from Cython.Build import cythonize
 
 HERE = pathlib.Path("winloop")
 _ROOT = pathlib.Path(__file__).parent
-
-
-with open(str(_ROOT / 'winloop' / '_version.py')) as f:
-    for line in f:
-        if line.startswith('__version__ ='):
-            _, _, version = line.partition('=')
-            VERSION = version.strip(" \n'\"")
-            break
-    else:
-        raise RuntimeError(
-            'unable to read the version from winloop/_version.py')
 
 
 def get_c_files():
@@ -38,91 +37,86 @@ def get_c_files():
 
 # This is a temporary test solution and is not the official file yet but this
 # is to display/show what I'm currently using to compile the winloop library...
-def do_installation():
-    try:
-        long_description = (HERE.parent / 'readme.md').open("r").read()
-    except Exception:
-        long_description = ""
+try:
+    long_description = (HERE.parent / 'readme.md').open("r").read()
+except Exception:
+    long_description = ""
 
-    # "Thank goodness we don't have to dig out the libpath ourselves I almost
-    # vomited" - Vizonex
-    Windows_Libraries = [
-        "Shell32.lib",
-        "Ws2_32.lib",
-        "Advapi32.lib",
-        "iphlpapi.lib",
-        "WSock32.lib",
-        "Userenv.lib",
-        "User32.lib",
-        "Dbghelp.lib",
-        "Ole32.lib"
-    ]
+# "Thank goodness we don't have to dig out the libpath ourselves I almost
+# vomited" - Vizonex
+Windows_Libraries = [
+    "Shell32.lib",
+    "Ws2_32.lib",
+    "Advapi32.lib",
+    "iphlpapi.lib",
+    "WSock32.lib",
+    "Userenv.lib",
+    "User32.lib",
+    "Dbghelp.lib",
+    "Ole32.lib"
+]
 
-    c_files = list(get_c_files())
+c_files = list(get_c_files())
 
-    ext = [
-        Extension(
-            "winloop.loop",
-            ["winloop\\loop.pyx"] + c_files,
-            include_dirs=["vendor/libuv/src", "vendor/libuv/src/win",
-                          "vendor/libuv/include"],
-            # NOTE uv_a.lib will be user-compiled when
-            # I've fixed the install there's still been some problems with
-            # compiling fs-poll.c on it's own...
-            extra_link_args=Windows_Libraries,  # "winloop\\vendor\\libuv.lib"
-            # I have some macros setup for now to help me
-            # with debugging -Vizonex
-            library_dirs=["winloop"],
-            define_macros=[
-                ("WIN32_LEAN_AND_MEAN", 1),
-                ("_WIN32_WINNT", "0x0602")
-            ]
-        )
-    ]
-
-    setup(
-        name="winloop",
-        author="Vizonex",
-        version=VERSION,
-        description="An alternative library for uvloop"
-                    "compatibility with Windows",
-        ext_modules=cythonize(ext),
-        license="MIT",
-        platforms=['Microsoft Windows'],
-        keywords=[
-            "winloop",
-            "libuv",
-            "windows",
-            "cython",
-            "fast-asyncio",
-            "uvloop-alternative"
-        ],
-        # Credit to godlygeek for helping me fix the setup.py file
-        packages=find_packages(include="winloop*"),
-
-        include_package_data=True,
-        long_description_content_type='text/markdown',
-        long_description=long_description,
-        classifiers=[
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: 3.11',
-            'Programming Language :: Python :: 3.12',
-            'Intended Audience :: Developers',
-            'Framework :: AsyncIO'
+ext = [
+    Extension(
+        "winloop.loop",
+        ["winloop\\loop.pyx"] + c_files,
+        include_dirs=["vendor/libuv/src", "vendor/libuv/src/win",
+                      "vendor/libuv/include"],
+        # NOTE uv_a.lib will be user-compiled when
+        # I've fixed the install there's still been some problems with
+        # compiling fs-poll.c on it's own...
+        extra_link_args=Windows_Libraries,
+        # I have some macros setup for now to help me
+        # with debugging -Vizonex
+        library_dirs=["winloop"],
+        define_macros=[
+            ("WIN32_LEAN_AND_MEAN", 1),
+            ("_WIN32_WINNT", "0x0602")
         ]
     )
+]
+
+with open(str(_ROOT / 'winloop' / '_version.py')) as f:
+    for line in f:
+        if line.startswith('__version__ ='):
+            _, _, version = line.partition('=')
+            VERSION = version.strip(" \n'\"")
+            break
+    else:
+        raise RuntimeError(
+            'unable to read the version from winloop/_version.py')
 
 
-if __name__ == "__main__":
-
-    if sys.platform not in ('win32', 'cygwin', 'cli'):
-        # TODO (Vizonex) Ask uvloop Owners/contributors if it would be ok or
-        # acceptable to recommend this library as an alternative resource for
-        # any users on windows
-        raise RuntimeError("Winloop is only available for Windows users."
-                           " Please try installing uvloop instead, you"
-                           " won't be disappointed with it...")
-
-    do_installation()
+setup(
+    name="winloop",
+    author="Vizonex",
+    version=VERSION,
+    description="Windows version of uvloop",
+    ext_modules=cythonize(ext),
+    keywords=[
+        "winloop",
+        "libuv",
+        "windows",
+        "fast-asyncio",
+        "uvloop-alternative"
+    ],
+    # Credit to godlygeek for helping me fix the setup.py file
+    packages=find_packages(include="winloop*"),
+    include_package_data=True,
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Windows',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Intended Audience :: Developers',
+        'Framework :: AsyncIO'
+    ]
+)
