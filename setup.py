5ae085ff1e49cf2b95fb52d8de589d48e4826453
Berry Schoenmakers <l.a.m.schoenmakers@tue.nl>
2024-05-30 16:25:20 +0200

    Fix Operating System classifier.

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ setup(
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: Windows',
+        'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
