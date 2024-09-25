8d3c60a136e51b420e3258e06882d877e650a6b8
Vizonex <114684698+Vizonex@users.noreply.github.com>
2024-09-25 17:13:10 -0500

    Create test_playwright.py

---
+++ tests/test_playwright.py
@@ -0,0 +1,33 @@
+try:
+    from playwright.async_api import async_playwright
+except ImportError:
+    skip_tests = True
+else:
+    skip_tests = False
+
+import unittest
+from winloop import _testbase as tb
+
+# SEE: https://github.com/Vizonex/Winloop/issues/34
+
+class _TestPlaywright:
+    def test_example_code(self):
+        # Example code is from microsoft's own example https://github.com/microsoft/playwright-python 
+        async def run():
+            async with async_playwright() as p:
+                for browser_type in [p.chromium, p.firefox, p.webkit]:
+                    browser = await browser_type.launch()
+                    page = await browser.new_page()
+                    await page.goto('http://playwright.dev')
+                    await page.screenshot(path=f'example-{browser_type.name}.png')
+                    await browser.close()
+
+        self.loop.run_until_complete(run())
+
+@unittest.skipIf(skip_tests, "no playwright module")
+class Test_UV_Playwright(_TestPlaywright, tb.UVTestCase):
+    pass
+
+@unittest.skipIf(skip_tests, "no playwright module")
+class Test_AIO_Playwright(_TestPlaywright, tb.AIOTestCase):
+    pass
